3da2086f2de285fd144d7a643896d3d34dc9329f
Brittany Ganser <15brganser@gmail.com>
2022-03-30 14:07:09 -0600

    generate html file

--- app.js
+++ app.js
@@ -1,5 +1,18 @@
+const fs = require("fs");
+//accepts function from page-template.js
+const generatePage = require("./src/page-template.js");
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+//assignment destructuring - new array we created from slice giving those a varaible name of name and github
+const [name, github] = profileDataArgs;
+
+//generate page is an async function here
+fs.writeFile("index.html", generatePage(name, github), (err) => {
+  if (err) throw err;
+
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
+
+/* console.log(profileDataArgs);
 
 const printProfileData = (profileDataArr) => {
   // This...
@@ -14,4 +27,4 @@ const printProfileData = (profileDataArr) => {
   profileDataArr.forEach((profileItem) => console.log(profileItem));
 };
 
-printProfileData(profileDataArgs);
+printProfileData(profileDataArgs); */
