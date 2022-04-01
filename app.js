b47df804856fa41d49ff05ffa9a8273773e767fa
Brittany Ganser <15brganser@gmail.com>
2022-04-01 13:25:56 -0600

    added validate function to questions

--- app.js
+++ app.js
@@ -8,11 +8,27 @@ const promptUser = () => {
       type: "input",
       name: "name",
       message: "What is your name?",
+      validate: (nameInput) => {
+        if (nameInput) {
+          return true;
+        } else {
+          console.log("Please enter your name!");
+          return false;
+        }
+      },
     },
     {
       type: "input",
       name: "github",
       message: "Enter your GitHub Username",
+      validate: (githubInput) => {
+        if (githubInput) {
+          return true;
+        } else {
+          console.log("Please enter your Github Username!");
+          return false;
+        }
+      },
     },
     {
       type: "input",
@@ -37,11 +53,27 @@ const promptProject = (portfolioData) => {
         type: "input",
         name: "name",
         message: "What is the name of your project?",
+        validate: (projectNameInput) => {
+          if (projectNameInput) {
+            return true;
+          } else {
+            console.log("Please enter your project name!");
+            return false;
+          }
+        },
       },
       {
         type: "input",
         name: "description",
         message: "Provide a description of the project (Required)",
+        validate: (descriptionInput) => {
+          if (descriptionInput) {
+            return true;
+          } else {
+            console.log("Please enter a project description!");
+            return false;
+          }
+        },
       },
       {
         type: "checkbox",
@@ -61,6 +93,14 @@ const promptProject = (portfolioData) => {
         type: "input",
         name: "link",
         message: "Enter the GitHub link to your project. (Required)",
+        validate: (linkInput) => {
+          if (linkInput) {
+            return true;
+          } else {
+            console.log("Please enter the Github link to your project!");
+            return false;
+          }
+        },
       },
       {
         type: "confirm",
